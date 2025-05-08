7a8ee74a5e57ca3894c2a99c00e2e97775631205
yangxiaosong <947352905@qq.com>
2025-05-08 19:50:51 +0800

    虚拟机

---
+++ vm.cpp
@@ -0,0 +1,87 @@
+#include "vm.h"
+#include <stdio.h>
+#include "debug.h"
+
+VM vm;
+void initVM()
+{
+    resetStack();
+}
+
+void freeVM()
+{
+}
+
+static void resetStack() {
+    vm.stackTop = vm.stack;
+}
+
+void push(Value value)
+{
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop()
+{
+    vm.stackTop--;
+    return *vm.stackTop;
+}
+
+InterpretResult interpret(Chunk* chunk)
+{
+	vm.chunk = chunk;
+	vm.ip = vm.chunk->code;
+	return run();
+}
+
+static InterpretResult run() {
+    #define READ_BYTE() \
+            *vm.ip++
+    #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+    #define BINARY_OP(op) \
+        do { \
+          double b = pop(); \
+          double a = pop(); \
+          push(a op b); \
+        } while (false)
+
+
+    for (;;) {
+        #ifdef DEBUG_TRACE_EXECUTION
+                printf("          stack:");
+                for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+                    printf("[ ");
+                    printValue(*slot);
+                    printf(" ]");
+                }
+                printf("\n");
+                disassembleInstruction(vm.chunk,
+                    (int)(vm.ip - vm.chunk->code));
+        #endif
+        uint8_t instruction;
+        switch (instruction = READ_BYTE()) {
+            case OP_CONSTANT: {
+                Value constant = READ_CONSTANT();
+                push(constant);
+                printValue(constant);
+                printf("\n");
+                break;
+            }
+            case OP_ADD:      BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE:   BINARY_OP(/); break;
+            case OP_NEGATE:   *(vm.stackTop - 1) = -*(vm.stackTop - 1);//push(-pop()); break;
+            case OP_RETURN: {
+                printValue(pop());
+                printf("\n");
+                return INTERPRET_OK;
+            }
+        }
+    }
+    #undef READ_BYTE
+    #undef READ_CONSTANT
+    #undef BINARY_OP
+}
+
---
+++ vm.h
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef clox_vm_h
+#define clox_vm_h
+
+#include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
+
+typedef struct {
+	Chunk* chunk;
+	uint8_t* ip;
+	Value stack[STACK_MAX];
+	Value* stackTop;
+} VM;
+
+typedef enum {
+	INTERPRET_OK,
+	INTERPRET_COMPILE_ERROR,
+	INTERPRET_RUNTIME_ERROR
+} InterpretResult;
+void initVM();
+void freeVM();
+
+static void resetStack();
+void push(Value value);
+Value pop();
+
+InterpretResult interpret(Chunk* chunk);
+static InterpretResult run();
+#endif
