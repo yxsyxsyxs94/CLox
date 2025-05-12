162a118f98cc48e80acb94f64379599d30538ffd
yangxiaosong <947352905@qq.com>
2025-05-12 21:01:18 +0800

    补全

--- compiler.h
+++ compiler.h
@@ -2,6 +2,13 @@
 #ifndef clox_compiler_h
 #define clox_compiler_h
 
-void compile(const char* source);
+#include "vm.h"
 
+bool compile(const char* source, Chunk* chunk);
+static void advance();
+static void errorAtCurrent(const char* message);
+static void error(const char* message);
+static void errorAt(Token* token, const char* message);
+static void consume(TokenType type, const char* message);
+static void emitByte(uint8_t byte);
 #endif
